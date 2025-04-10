9d7b327a9bcb01f61e03b2e971df7e5f2f5bf0bb
ABID066 <mdabid1152@gmail.com>
2025-04-10 07:57:07 +0600

    contact-page DONE

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import ContactForm from '../component/contact/ContactForm';
 
-const ContactPage = () => {
+export default function ContactPage() {
     return (
-        <div>
-            <h1>Contact Page</h1>
-        </div>
-    );
-};
+        <>
+            {/* Page Header */}
+            <div className="bg-[#F0EDFD] py-30 text-center">
+                <h1 className="text-3xl md:text-4xl font-bold text-gray-900 pt-20">Contact Us</h1>
+                <p className="text-sm text-gray-600 mt-2">
+                    <a href="/" className="text-gray-500 hover:underline">Home</a> &gt; <span className="text-gray-700">Contact Us</span>
+                </p>
+            </div>
 
-export default ContactPage;
+            {/* Main Content Container */}
+            <div className=" max-w-6xl mx-auto px-4 py-35">
+                <div className="flex justify-center">
+                    <ContactForm />
+                </div>
+            </div>
+        </>
+    );
+}
