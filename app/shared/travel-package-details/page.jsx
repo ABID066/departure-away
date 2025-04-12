472876e6e95e243c70b20d70012402467c6db3f1
ABID066 <mdabid1152@gmail.com>
2025-04-12 15:58:30 +0600

    edit in navbar

--- app/shared/travel-package-details/page.jsx
+++ app/shared/travel-package-details/page.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Page = () => {
     return (
         <div>
-            <h1>travel-package-details Page</h1>
+            <h1 >travel-package-details Page</h1>
         </div>
     );
 };
