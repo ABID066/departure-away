215c9a148afdde48eb2a5df6bf13b24633233221
Your Name <islamshamim1819@gmail.com>
2025-04-13 19:42:16 +0600

    category issue fixed

--- public/components/home/TopCategoryCard.jsx
+++ public/components/home/TopCategoryCard.jsx
@@ -1,25 +1,19 @@
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
-import img from "@/public/images/hiking-v1.png"
+import img from "@/public/images/hiking-v1.png";
 
 export default function TopCategoryCard({ category }) {
-  const { name, image } = category;
-  
+  const { name } = category;
+
   return (
-    <div className="card h-44 relative rounded-lg overflow-hidden shadow-sm">
+    <div className="card relative rounded-lg shadow-sm">
       <Link href="#">
-        <div className="relative h-full bg-blue-500 w-full">
-          <Image
-            src={image}
-            alt={name}
-            fill
-            className="object-cover"
-          />
-          <div className="absolute inset-0 bg-black bg-opacity-20"></div>
-          <div className="absolute bottom-4 text-center w-full">
-            <h2 className="font-semibold text-white text-xl md:text-2xl">{name}</h2>
-          </div>
+        <div className="relative overflow-hidden rounded-lg">
+          <Image src={img} alt={name} className="w-full h-64 object-cover" />
+        </div>
+        <div className="relative -top-15 text-white">
+          <h2 className="font-semibold  text-xl md:text-2xl">{name}</h2>
         </div>
       </Link>
     </div>
