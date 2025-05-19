41ac32082d85ca7adaa891c6c425a323df361c66
md-nasim-mondal <nasim.programmer24@gmail.com>
2025-05-19 11:20:20 +0600

    update folder structure

---
+++ app/exclusive-offers/[id].tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ExclusiveOfferDetails = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default ExclusiveOfferDetails;
---
+++ app/travel-packages/[id].tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TravelPackageDetails = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default TravelPackageDetails;
