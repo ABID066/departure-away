48b367bb1c571dbe1c746eb55faccfd770e0e553
ABID066 <mdabid1152@gmail.com>
2025-05-22 21:03:19 +0600

    update backend api

--- app/signUp/page.jsx
+++ app/signUp/page.jsx
@@ -111,7 +111,7 @@ export default function Page() {
 
     try {
       // Make API call to create user
-      const response = await fetch('http://localhost:5000/api/v1/user/create-user', {
+      const response = await fetch('https://royolex.vercel.app/api/v1/user/create-user', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
